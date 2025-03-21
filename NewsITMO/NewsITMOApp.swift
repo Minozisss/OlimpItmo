69dec9bae224939f7d1a4913f4ef4a1bddd00a23
Minozisss <kudryavtsev.ma.03@gmail.com>
2025-03-21 15:26:43 +0300

    Initial Commit

---
+++ NewsITMO/NewsITMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsITMOApp.swift
+//  NewsITMO
+//
+//  Created by Максим Кудрявцев on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsITMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
